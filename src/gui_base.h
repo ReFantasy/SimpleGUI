36d6eeefc497bdf0429017344d9de53452217552
ReFantasy <309741309@qq.com>
2022-06-20 19:05:27 +0800

    添加配置文件

--- src/gui_base.h
+++ src/gui_base.h
@@ -4,19 +4,24 @@
 
 #ifndef GUI_GUI_BASE_H
 #define GUI_GUI_BASE_H
-#include "Eigen/Core"
 #include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
 #include "glsl_shader.h"
-#include <GLFW/glfw3.h>
 #include <iostream>
 #include <memory>
 #include <string>
 
+// 窗口默认宽度
+#define WIDTH_DEFAULT 600
+// 默认窗口高度
+#define HEIGHT_DEFAULT 600
+
 class GUIBase
 {
   public:
-    explicit GUIBase(int width = 600, int height = 600, std::string title = "OpenGL Window",
-                     Eigen::Vector4f bg_color = Eigen::Vector4f{0.067f, 0.184f, 0.255f, 1.0f})
+    explicit GUIBase(int width = WIDTH_DEFAULT, int height = HEIGHT_DEFAULT, std::string title = "OpenGL Window",
+                     glm::vec4 bg_color = glm::vec4{0.067f, 0.184f, 0.255f, 1.0f})
         : _width(width), _height(height), _window_title(std::move(title)), _bg_color(std::move(bg_color))
     {
         InitOpenGL();
@@ -30,11 +35,11 @@ class GUIBase
   protected:
     GLFWwindow *_window_id = nullptr;
 
-    int _width = 600;
-    int _height = 600;
+    int _width = WIDTH_DEFAULT;
+    int _height = HEIGHT_DEFAULT;
 
     // background color
-    Eigen::Vector4f _bg_color{0, 0, 0, 1};
+    glm::vec4 _bg_color{};
 
     // title of window
     std::string _window_title = std::string("OpenGL Window");
