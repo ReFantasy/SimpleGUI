7613400531a274d20ecdcfca323bdefe6f5139c9
ReFantasy <309741309@qq.com>
2022-06-22 02:29:35 +0800

    add model

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+#include "src/ext/model.h"
 #include "src/gui/gui.h"
 #include "src/gui/mesh.h"
+#include <filesystem>
 #include <memory>
-#include <random>
 #include <numeric>
+#include <random>
 template <typename T, typename Distribution = std::uniform_real_distribution<T>>
 T RandomNumber(const T &lo = 0, const T &hi = 1)
 {
@@ -19,6 +21,8 @@ std::shared_ptr<Sphere> CreateSphere();
 std::shared_ptr<Mesh> cube;
 std::shared_ptr<Mesh> CreateCube();
 
+std::shared_ptr<Model> model;
+
 class GUI3D : public GUI
 {
   public:
@@ -30,30 +34,32 @@ class GUI3D : public GUI
         _shader.deactivate();
 
         ground->Draw(this->_shader.GetShaderID());
-
-
         cube->Draw(this->_shader.GetShaderID());
 
+        //model->Draw(this->_shader.GetShaderID());
     }
 };
 
 int main(int argc, char *argv[])
 {
+    std::filesystem::path source_dir(PROJECT_SOURCE_DIR);
+
     GUI3D gui(800, 600);
-    gui.SetBackGroundColor({0,0,0,0});
+    gui.SetBackGroundColor({0, 0, 0, 0});
     GUI3D::SetVsync();
 
-    gui.GetCamera().UpdateCamPos(glm::vec3{1.3,1.6,2.0}).UpdateCamTar(glm::vec3(0,0.5,0));
-    gui.GetShader().LoadShaderFromFile("/Users/refantasy/Desktop/SimpleGUI/src/gui/basic_vs.glsl",
-                                       "/Users/refantasy/Desktop/SimpleGUI/src/gui/basic_fs.glsl");
-
-
+    gui.GetCamera().UpdateCamPos(glm::vec3{0.5, 1.6, 2.0}).UpdateCamTar(glm::vec3(0, 0.5, 0));
+    gui.GetShader().LoadShaderFromFile(std::string(source_dir.string()+"/src/gui/basic_vs.glsl").c_str(),
+                                       std::string(source_dir.string()+"/src/gui/basic_fs.glsl").c_str());
 
     cube = CreateCube();
-    cube->LoadDiffuseMap("/Users/refantasy/Desktop/SimpleGUI/container2.png");
-    cube->LoadSpecularMap("/Users/refantasy/Desktop/SimpleGUI/container2_specular.png");
+    cube->LoadDiffuseMap(std::string(source_dir.string()+"/resource/container2.png").c_str());
+    cube->LoadSpecularMap(std::string(source_dir.string()+"/resource/container2_specular.png").c_str());
 
     ground = CreateGround();
+
+    model = std::make_shared<Model>(std::string(source_dir.string()+"/resource/NasaShuttle/shuttle.obj"));
+    model->meshes[0].LoadDiffuseMap(std::string(source_dir.string()+"/resource/NasaShuttle/spstob_1.jpg"));
 
     gui.Show();
 
@@ -62,25 +68,23 @@ int main(int argc, char *argv[])
 
 std::shared_ptr<Mesh> CreateGround()
 {
-    Vertex v1,v2,v3,v4;
+    Vertex v1, v2, v3, v4;
     float max_value = 10;
-    v1.Position = glm::vec3 (max_value,0.0f,max_value);
-    v2.Position = glm::vec3 (max_value,0.0f,-max_value);
-    v3.Position = glm::vec3 (-max_value,0.0f,-max_value);
-    v4.Position = glm::vec3 (-max_value,0.0f,max_value);
+    v1.Position = glm::vec3(max_value, 0.0f, max_value);
+    v2.Position = glm::vec3(max_value, 0.0f, -max_value);
+    v3.Position = glm::vec3(-max_value, 0.0f, -max_value);
+    v4.Position = glm::vec3(-max_value, 0.0f, max_value);
 
-    glm::vec3  normal(0,1,0);
-    v1.Normal = v2.Normal=v3.Normal = v4.Normal = normal;
+    glm::vec3 normal(0, 1, 0);
+    v1.Normal = v2.Normal = v3.Normal = v4.Normal = normal;
 
+    v1.Color = glm::vec3(RandomNumber<float>(0, 1), RandomNumber<float>(0, 1), RandomNumber<float>(0, 1));
+    v2.Color = glm::vec3(RandomNumber<float>(0, 1), RandomNumber<float>(0, 1), RandomNumber<float>(0, 1));
+    v3.Color = glm::vec3(RandomNumber<float>(0, 1), RandomNumber<float>(0, 1), RandomNumber<float>(0, 1));
+    v4.Color = glm::vec3(RandomNumber<float>(0, 1), RandomNumber<float>(0, 1), RandomNumber<float>(0, 1));
 
-
-    v1.Color = glm::vec3 (RandomNumber<float>(0,1),RandomNumber<float>(0,1),RandomNumber<float>(0,1));
-    v2.Color = glm::vec3 (RandomNumber<float>(0,1),RandomNumber<float>(0,1),RandomNumber<float>(0,1));
-    v3.Color = glm::vec3 (RandomNumber<float>(0,1),RandomNumber<float>(0,1),RandomNumber<float>(0,1));
-    v4.Color = glm::vec3 (RandomNumber<float>(0,1),RandomNumber<float>(0,1),RandomNumber<float>(0,1));
-
-    std::vector<Vertex> vertexes{v1,v2,v3,v4};
-    std::vector<unsigned int> indices{0,1,3,1,2,3};
+    std::vector<Vertex> vertexes{v1, v2, v3, v4};
+    std::vector<unsigned int> indices{0, 1, 3, 1, 2, 3};
 
     auto g = std::make_shared<Mesh>(vertexes, indices);
     g->GenGLBuffers();
@@ -88,17 +92,16 @@ std::shared_ptr<Mesh> CreateGround()
 }
 std::shared_ptr<Sphere> CreateSphere()
 {
-    Vertex s1,s2,s3;
-    s1.Position = glm::vec3 (RandomNumber<float>(-8,8),RandomNumber<float>(1,5),RandomNumber<float>(-8,8));
-    s1.Color = glm::vec3 (1,0,0);
-    s2.Position = glm::vec3 (RandomNumber<float>(-8,8),RandomNumber<float>(1,5),RandomNumber<float>(-8,8));
-    s2.Color = glm::vec3 (0,1,0);
-    s3.Position = glm::vec3 (RandomNumber<float>(-8,8),RandomNumber<float>(1,5),RandomNumber<float>(-8,8));
-    s3.Color = glm::vec3 (0,0,1);
+    Vertex s1, s2, s3;
+    s1.Position = glm::vec3(RandomNumber<float>(-8, 8), RandomNumber<float>(1, 5), RandomNumber<float>(-8, 8));
+    s1.Color = glm::vec3(1, 0, 0);
+    s2.Position = glm::vec3(RandomNumber<float>(-8, 8), RandomNumber<float>(1, 5), RandomNumber<float>(-8, 8));
+    s2.Color = glm::vec3(0, 1, 0);
+    s3.Position = glm::vec3(RandomNumber<float>(-8, 8), RandomNumber<float>(1, 5), RandomNumber<float>(-8, 8));
+    s3.Color = glm::vec3(0, 0, 1);
 
-
-    std::vector<Vertex> s_v{s1,s2,s3};
-    auto s = std::make_shared<Sphere>(s_v, 3,48);
+    std::vector<Vertex> s_v{s1, s2, s3};
+    auto s = std::make_shared<Sphere>(s_v, 3, 48);
     s->GenGLBuffers();
 
     return s;
@@ -107,58 +110,39 @@ std::shared_ptr<Mesh> CreateCube()
 {
     float vertices[] = {
         // positions          // normals           // texture coords
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
-        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
-        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
-        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f, 0.0f, 0.0f, 0.5f,  -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f, 1.0f, 0.0f,
+        0.5f,  0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f, 1.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f, 1.0f, 1.0f,
+        -0.5f, 0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f, 0.0f, 0.0f,
 
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
-        0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f, 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f,
+        -0.5f, 0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 1.0f, -0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f,
 
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f, 0.5f,  0.5f,  -1.0f, 0.0f,  0.0f,  1.0f, 0.0f, -0.5f, 0.5f,  -0.5f, -1.0f, 0.0f,  0.0f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f, 0.0f,  0.0f,  0.0f, 1.0f, -0.5f, -0.5f, -0.5f, -1.0f, 0.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f,  -1.0f, 0.0f,  0.0f,  0.0f, 0.0f, -0.5f, 0.5f,  0.5f,  -1.0f, 0.0f,  0.0f,  1.0f, 0.0f,
 
-        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
-        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-        0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.5f,  0.5f,  -0.5f, 1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+        0.5f,  -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,  0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        0.5f,  -0.5f, 0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
 
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
-        0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
-        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,  0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,  1.0f, 1.0f,
+        0.5f,  -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f,  1.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f,  0.0f, 0.0f, -0.5f, -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,  0.0f, 1.0f,
 
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-        0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
-    };
+        -0.5f, 0.5f,  -0.5f, 0.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f, 0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f,  1.0f,  0.0f,  0.0f, 1.0f};
 
     std::vector<Vertex> VV;
     std::vector<unsigned int> indices;
-    for(int i = 0;i<36;i++)
+    for (int i = 0; i < 36; i++)
     {
         Vertex v;
-        int base = 8*i;
-        v.Position = glm::vec3 (vertices[base+0],vertices[base+1],vertices[base+2]) + glm::vec3(0,0.6,0);
-        v.Normal = glm::vec3 (vertices[base+3],vertices[base+4],vertices[base+5]);
-        v.Texcoord = glm::vec2(vertices[base+6],vertices[base+7]);
+        int base = 8 * i;
+        v.Position = glm::vec3(vertices[base + 0], vertices[base + 1], vertices[base + 2]) + glm::vec3(0, 0.6, 0);
+        v.Normal = glm::vec3(vertices[base + 3], vertices[base + 4], vertices[base + 5]);
+        v.Texcoord = glm::vec2(vertices[base + 6], vertices[base + 7]);
         VV.push_back(v);
         indices.push_back(i);
     }
