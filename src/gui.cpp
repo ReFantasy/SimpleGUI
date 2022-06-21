dd8072f989e72ec9545eb503b8ba007f47932100
ReFantasy <309741309@qq.com>
2022-06-21 13:12:26 +0800

    comment

--- src/gui.cpp
+++ src/gui.cpp
@@ -5,13 +5,14 @@
 #include "gui.h"
 void GUI::BaseRender()
 {
+    // 计算投影矩阵并传递给着色器
     int width, height;
     glfwGetWindowSize(_window_id, &width, &height);
-
     _shader.activate();
     auto mvp = cam.GetPerspectiveMatrix((float)width / (float)height) * cam.GetViewMatrix();
     _shader.setMat4("MVP", mvp);
     _shader.deactivate();
 
+    // 用户渲染
     Render();
 }
