9703eacb04f49b2b689e412183bc653c311d491e
ReFantasy <309741309@qq.com>
2022-06-23 05:05:48 +0800

    .

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <filesystem>
 #include <memory>
 #include <random>
+
+
 template <typename T, typename Distribution = std::uniform_real_distribution<T>>
 
 T RandomNumber(const T &lo = 0, const T &hi = 1)
